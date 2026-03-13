1323b0e71ca685b8e5e655ee719abc6b2d5cedb0
Rajesh Sahu <ishurwara654@gmail.com>
2026-03-13 16:13:17 +0530

    Add files via upload

---
+++ src/main/java/base/BaseTest.java
@@ -0,0 +1,25 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
+
+public class BaseTest {
+
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+//        WebDriverManager.chromedriver().setup();
+      
+    	driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.myntra.com/");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
